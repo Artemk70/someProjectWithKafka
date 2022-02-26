b9d660586b317f0a745ce2692c676e300bc19600
Кот Артем <artem70107@gmail.com>
2022-02-26 15:17:43 +0300

    initial commit

---
+++ src/main/java/com/aplication/someprojectwithkafka/SomeProjectWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.someprojectwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeProjectWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeProjectWithKafkaApplication.class, args);
+	}
+
+}
