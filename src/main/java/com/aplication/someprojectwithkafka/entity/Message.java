5c069a3b9f84e48c105644d807240d71c471a2ba
Кот Артем <artem70107@gmail.com>
2022-02-26 15:55:15 +0300

    set up the project

---
+++ src/main/java/com/aplication/someprojectwithkafka/entity/Message.java
@@ -0,0 +1,27 @@
+package com.aplication.someprojectwithkafka.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "messages")
+public class Message implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "date_of_send", nullable = false)
+    private LocalDateTime dateOfSend;
+
+    @Column(nullable = false)
+    private String text;
+}
